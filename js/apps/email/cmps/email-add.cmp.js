7a35fb085c81c46c2f5ff240b36d1c9b9ef3629b
kostagon <49278316+kostagon@users.noreply.github.com>
2019-11-06 13:07:35 +0200

    added folders

---
+++ js/apps/email/cmps/email-add.cmp.js
@@ -0,0 +1,8 @@
+export default {
+    name: 'email-add',
+    template: `
+        <section class="email-add-container">
+            <h1>Email add</h1>
+        </section>
+    `
+}
---
+++ js/apps/email/cmps/email-list.cmp.js
@@ -0,0 +1,8 @@
+export default {
+    name: 'email-list',
+    template: `
+        <section class="email-list-container">
+            <h1>Email list</h1>
+        </section>
+    `
+}
